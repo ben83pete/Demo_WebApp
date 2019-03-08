3d097499815910656b8ec5aa00d3f509c84e60f7
ben Petersen <ben83pete@gmail.com>
2019-03-08 13:39:56 -0500

    init

---
+++ WebApplication2/Models/SalesDb.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models {
+    public class SalesDb : DbContext {
+
+        public DbSet<Product> Products { get; set; }
+
+        public static implicit operator Database(SalesDb v) {
+            throw new NotImplementedException();
+        }
+    }
+}
